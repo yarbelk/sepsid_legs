20ba76d5a1f38538d258e7e3fe736044ef91bd7b
yarbelk <james.rivettcarnac@gmail.com>
2014-02-24 11:26:19 +0800

    renamed script to have .py for windows

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@
 from setuptools import setup
 
 setup(name="sepsid_legs",
-        version='0.1.0',
+        version='0.1.1',
         description="A series of tools for the ananlysis of complexity of sepsid legs",
         author='James Rivett-Carnac',
         author_email='james.rivettcarnac@gmail.com',
         packages=['morpho_complexity',],
         install_requires=['path.py', 'pillow'],
-        scripts=['scripts/sl_resize',],
+        scripts=['scripts/sl_resize.py',],
         test_suite= 'nose.collector',
         test_requires = ['nose',]
         )
